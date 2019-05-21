fd238356a39822d87f4408072512c74ed646b59b
Trond Kandal <Trond.Kandal@ntnu.no>
2019-05-21 19:29:00 +0200

    More doc.

--- pinger.go
+++ pinger.go
@@ -1,7 +1,8 @@
+// Package dbping is kind of a convenience package to provide a pinger for database-drivers that don't implement
+// a ping-method.
 package dbping
 
-// PingCloser is the basic interface for pinging  an database with a custom query and closing it afterwards.
+// Pinger is the basic interface for pinging  an database with a custom query and closing it afterwards.
 type Pinger interface {
 	Ping(string, map[string]interface{}) error
 }
-
